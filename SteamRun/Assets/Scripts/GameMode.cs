725f679eaf5bab1dbbb0732f6d738a7fa77d3cf8
Samuel-HB <sam.hb2007@gmail.com>
2026-03-03 16:24:02 +0100

    Added event to restart level (and BOTH old input system and new are usable)
    BOTH INPUT SYSTEMS ARE WORKING (project settings changed)

---
+++ SteamRun/Assets/Scripts/GameMode.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameMode : MonoBehaviour
+{
+    //inutile pour l'instant le game mode
+
+    private void Start()
+    {        
+    }
+}
