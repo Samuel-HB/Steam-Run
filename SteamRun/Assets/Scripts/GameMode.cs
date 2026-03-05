01cf10608d2e45b1234a1c9d0f8271f341704374
Samuel-HB <sam.hb2007@gmail.com>
2026-03-05 16:15:57 +0100

    Added timers to wait before restart level and wait for the play to be able to use inputs

--- SteamRun/Assets/Scripts/GameMode.cs
+++ SteamRun/Assets/Scripts/GameMode.cs
@@ -6,10 +6,10 @@ public class GameMode : MonoBehaviour
 {
     private GameObject player;
     [SerializeField] private GameObject playerPrefab;
-    [SerializeField] private Transform startPosition;
 
-    [SerializeField] private SceneAsset nextLevel;
+    [SerializeField] private Transform startPosition;
     [SerializeField] private Transform endLevelPoint;
+    [SerializeField] private SceneAsset nextLevel;
 
 
     private void Start()
@@ -25,11 +25,6 @@ public class GameMode : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Y)) // quand joueur mort ou niveau stoppé
         {
             EventManager.Instance.PlayerDeathFunc();
-        }
-
-        if (Input.GetKeyDown(KeyCode.U))
-        {
-            EventManager.Instance.StartLevelFunc();
         }
     }
 
---
+++ SteamRun/Assets/Scripts/TimerRestartLevel.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using UnityEngine;
+
+public class TimerRestartLevel : MonoBehaviour
+{
+    private IEnumerator timer;
+
+    private void Start()
+    {
+        EventManager.Instance.PlayerDeath += CallStartTimer;
+    }
+
+    public void CallStartTimer()
+    {
+        timer = StartTimer();
+        StartCoroutine(timer);
+    }
+
+    public void StopTimer()
+    {
+        if (timer != null)
+        {
+            StopCoroutine(timer);
+            timer = null;
+        }
+    }
+
+    IEnumerator StartTimer()
+    {
+        for (int tenthSecond = 0; tenthSecond <= 6; tenthSecond++) {
+            yield return new WaitForSeconds(0.1f);
+        }
+        EventManager.Instance.StartLevelFunc();
+        StopTimer();
+    }
+
+    private void OnDestroy()
+    {        
+        EventManager.Instance.PlayerDeath -= CallStartTimer;
+    }
+}
