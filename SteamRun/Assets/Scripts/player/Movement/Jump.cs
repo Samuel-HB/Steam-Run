af1e272eb7e9f9debf66cf5727d08df8ffde2463
MaxenceDumont33 <maxence.dumont2007@gmail.com>
2026-04-08 15:11:58 +0200

    new movement system

--- SteamRun/Assets/Scripts/player/Movement/Jump.cs
+++ SteamRun/Assets/Scripts/player/Movement/Jump.cs
@@ -1,16 +1,138 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Jump : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    bool isGrounded = false;
+    bool isWallSliding;
+    public GameObject groundCollider;
+    public GameObject rightWallCollider;
+    public GameObject leftWallCollider;
+    public Rigidbody2D playerRb;
+
+    [Header("Jump")]
+    public float jumpForce;
+    private float jumpForceRef;
+    private bool isJumping;
+    private bool isRightWallJumping;
+    private bool isLeftWallJumping;
+    public bool isLeftWallSliding;
+    public bool isRightWallSliding;
+    public float wallFriction;
+    public float wallJumpStrength;
+    private float wallJumpStrengthRef;
     void Start()
     {
-        
+        jumpForceRef = jumpForce;
+        wallJumpStrengthRef = wallJumpStrength;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        RaycastHit2D ground = Physics2D.Raycast(groundCollider.transform.position, Vector2.right, 0.85f);
+        if (ground == true)
+        {
+            isGrounded = true;
+            
+        }
+        else if (ground == false)
+        {
+            isGrounded = false;           
+        }
+
+        RaycastHit2D rightWall = Physics2D.Raycast(rightWallCollider.transform.position, Vector2.up, 0.85f);
+        if (rightWall == true)
+        {
+            isWallSliding = true;
+            if (ground == false && playerRb.linearVelocityY < 0.1)
+            {
+                playerRb.linearVelocityY = 0f;
+                isRightWallSliding = true;
+            }
+        }
+        else 
+        {
+            isRightWallSliding = false;
+        }
+
+            RaycastHit2D leftWall = Physics2D.Raycast(leftWallCollider.transform.position, Vector2.up, 0.85f);
+        if (leftWall == true)
+        {
+            isWallSliding = true;
+            if (ground == false && playerRb.linearVelocityY <0.1)
+            {
+                playerRb.linearVelocityY = 0f;
+                isLeftWallSliding = true;
+            }           
+        }
+        else
+        {
+            isLeftWallSliding = false;
+        }
+        if(leftWall == false && rightWall == false)
+        {
+            isWallSliding = false;
+        }
+        if (isWallSliding && isGrounded==false && playerRb.linearVelocityY <0.1)
+        {
+            playerRb.AddForceY(wallFriction);
+        }
+        if (isJumping|| isRightWallJumping||isLeftWallJumping)
+        {
+            if (jumpForce > 0)
+            {
+                playerRb.AddForceY(jumpForce);
+                jumpForce -= 0.1f;
+            }
+            if (isRightWallJumping)
+            {
+                if (wallJumpStrength > 0)
+                {
+                    playerRb.AddForce(wallJumpStrength * Vector2.left);
+                    wallJumpStrength -= 0.1f;
+                    print(wallJumpStrength);
+                }
+            }
+            if (isLeftWallJumping)
+            {
+                if (wallJumpStrength > 0)
+                {
+                    playerRb.AddForce(wallJumpStrength * Vector2.right);
+                    wallJumpStrength -= 0.1f;
+                    print(wallJumpStrength);
+                }
+            }
+        }
+    }
+
+    public void StartJump(InputAction.CallbackContext _context)
+    {
+        if (_context.started)
+        {
+            if (isGrounded)
+            {
+                jumpForce = jumpForceRef;
+                isJumping = true;
+            }
+            if (isGrounded == false && isWallSliding)
+            {
+                jumpForce = jumpForceRef;
+                wallJumpStrength = wallJumpStrengthRef;
+                if (isLeftWallSliding)
+                {
+                    isLeftWallJumping = true;
+                }
+                if (isRightWallSliding)
+                {
+                    isRightWallJumping = true;
+                }
+            }
+        }
+        else if (_context.canceled)
+        {
+            isJumping = false;
+            isLeftWallJumping = false;
+            isRightWallJumping = false;
+        }
     }
 }
--- SteamRun/Assets/Scripts/player/Movement/Move.cs
+++ SteamRun/Assets/Scripts/player/Movement/Move.cs
@@ -1,31 +1,72 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.UI;
 
 public class Move : MonoBehaviour
 {
     [SerializeField] float speed;
+    [SerializeField] float sprintSpeed;
+    private float moveSpeed;
     [SerializeField] float maxSpeed;
+    public Rigidbody2D rb;
+    public float Acceleration;
+    public float deceleration;
+    public float velPower;
+    public float frictionAmount;
 
-    private Vector2 inputDirection;
+    [SerializeField]
+    private Jump jumpRef;
 
-    // Update is called once per frame
+    private float inputDirection;
+    private float inputDirectionRef;
 
     private void Start()
     {
-        //SC_InputManager.instance.OnMovement += SetInputDirection();
-    }
-    private void OnDestroy()
-    {
-        
+        moveSpeed = speed;
     }
     private void FixedUpdate()
     {
-        
+        if(jumpRef.isLeftWallSliding ==true && inputDirection < -0.1f)
+        {
+            inputDirection = 0;
+        }
+        else if (jumpRef.isRightWallSliding ==true && inputDirection> 0.1f)
+               {
+                   inputDirection = 0;
+               }
+        else
+        {
+            inputDirection = inputDirectionRef;
+        }
+            float targetSpeed = inputDirection * moveSpeed;
+        float speedDif = targetSpeed - rb.linearVelocityX;
+        float accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? Acceleration : deceleration;
+        float movement = Mathf.Pow(Mathf.Abs(speedDif) * accelRate, velPower) * Mathf.Sign(speedDif);
+        rb.AddForce(movement * Vector2.right);
     }
 
-    private void SetInputDirection(InputAction.CallbackContext _context)
+    public void SetInputDirection(InputAction.CallbackContext _context)
     {
+        if (_context.started)
+        {
+            inputDirection = _context.ReadValue<float>();
+            inputDirectionRef = _context.ReadValue<float>();
+        }
+        else if (_context.canceled)
+        {
+            inputDirection = 0;
+            inputDirectionRef = 0;
+        }
+    }
 
+    public void Sprint(InputAction.CallbackContext _context)
+    {
+        if (_context.started)
+        {
+            moveSpeed = sprintSpeed;
+        }
+        else if (_context.canceled)
+        {
+            moveSpeed = speed;
+        }
     }
 }
