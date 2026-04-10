ce8cf427ffd30f3fef17a42419cc4b95cca541f9
Samuel-HB <sam.hb2007@gmail.com>
2026-04-10 13:50:07 +0200

    Added Main menu

--- SteamRun/Assets/Scripts/player/ActivateDeactivateAnim.cs
+++ SteamRun/Assets/Scripts/player/ActivateDeactivateAnim.cs
@@ -6,27 +6,12 @@ public class ActivateDeactivateAnim : MonoBehaviour
 
     public void RestartAnim()
     {
-        //animator.enabled = false;
-
         animator.SetBool("isRunning", false);
         animator.SetBool("isGripping", false);
         animator.Play("Idle", -1, 0);
-        //animator.Play("Exit", 0);
 
         //to restart animator and avoid constant animation blend
-        animator.enabled = false;
-        animator.enabled = true;
+        //animator.enabled = false;
+        //animator.enabled = true;
     }
-
-    //public void RestartAnim2()
-    //{
-    //    animator.Update(0f);
-    //    animator.CrossFade("Idle", 0f);
-
-    //    animator.SetBool("isRunning", false);
-    //    animator.SetBool("isGripping", false);
-    //    animator.Play("Idle", -1, 0);
-
-    //    animator.enabled = true;
-    //}
 }
