bf22daf1b98bae9d66149f3b4c454f1668f069a0
Samuel-HB <sam.hb2007@gmail.com>
2026-04-16 17:01:05 +0200

    Added background wheels for all levels in world1 and world3

--- SteamRun/Assets/Scripts/Camera/CameraManager.cs
+++ SteamRun/Assets/Scripts/Camera/CameraManager.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class CameraManager : MonoBehaviour
 {
-    [SerializeField] private GameMode gm;
+    //[SerializeField] private GameMode gm;
     [SerializeField] private Transform cameraTransform;
     public Transform playerTransform;
     private float camPositionX = 0;
